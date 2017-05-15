32fe4ba17a60cb43da65cba42c89bf63d827ffa6
dengle3 <dengle3@wanda.cn>
2017-05-15 10:22:00 +0800

    add

--- app/src/main/java/com/example/dengle/wealthandroid/module/main/MainActivity.java
+++ app/src/main/java/com/example/dengle/wealthandroid/module/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.dengle.wealthandroid;
+package com.example.dengle.wealthandroid.module.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.example.dengle.wealthandroid.R;
 
 public class MainActivity extends Activity {
 
